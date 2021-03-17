d0bddbae43b16e003f02c61b69eed5d8c0e17a81
alexei-kouprianov <alexei.kouprianov@gmail.com>
2021-03-18 00:23:09 +0300

    Updated to 2021-03-18 00:15:07 MSK;

--- scripts/yt_plotter.r
+++ scripts/yt_plotter.r
@@ -730,5 +730,5 @@ axis(2)
 dev.off()
 
 print(yt.ls[[1]]$COUNTS[nrow(yt.ls[[1]])] - yt.ls[[2]]$COUNTS[nrow(yt.ls[[2]])])
-print(paste("Updated to", tail(yt.ls[[6]]$TIME, 1), "MSK;"))
+print(paste("Updated to", tail(yt.ls[[6]]$TIME, 2), "MSK;"))
 # source("yt_plotter.r")
